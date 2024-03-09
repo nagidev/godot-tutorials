f4ba91902308414236985b4fe1a6b241bb769242
nagidev <bhagat.nagi@gmail.com>
2024-03-09 23:20:25 +0530

    Added gameover screen

--- Player/player.gd
+++ Player/player.gd
@@ -19,34 +19,33 @@ class_name Player
 
 @export_subgroup("Health")
 @export var fall_damage_threshold = 20
+@export var fall_damage_multiplier = 1
 
 @onready var head = $Head
 @onready var collision_shape = $CollisionShape3D
 @onready var top_cast = $TopCast
-@onready var hurt_overlay = $HurtOverlay
-@onready var health_bar = $HealthBar
-@onready var health_bar_bg = $HealthBarBG
+@onready var ui = $UI
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var look_rot : Vector2
 var stand_height : float
 var old_vel : float = 0.0
 var hurt_tween : Tween
+var moving : bool = true
 
 
 func _ready():
 	stand_height = collision_shape.shape.height
-	health_bar.value = GameState.get_value("health")
-	health_bar_bg.value = health_bar.value
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 func _physics_process(delta):
+	# movement
 	var move_speed = speed
 	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	else:
+	elif moving:
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = jump
 		elif Input.is_action_pressed("crouch") or top_cast.is_colliding():
@@ -57,7 +56,7 @@ func _physics_process(delta):
 
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	if direction and moving:
 		velocity.x = lerp(velocity.x, direction.x * move_speed, accel * delta)
 		velocity.z = lerp(velocity.z, direction.z * move_speed, accel * delta)
 	else:
@@ -66,6 +65,7 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
+	# rotation
 	var plat_rot = get_platform_angular_velocity()
 	look_rot.y += rad_to_deg(plat_rot.y * delta)
 	head.rotation_degrees.x = look_rot.x
@@ -75,13 +75,13 @@ func _physics_process(delta):
 	if old_vel < 0:
 		var diff = velocity.y - old_vel
 		if diff > fall_damage_threshold:
-			hurt(diff - fall_damage_threshold)
+			hurt((diff - fall_damage_threshold) * fall_damage_multiplier)
 			crouch(delta)
 	old_vel = velocity.y
 
 
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and moving:
 		look_rot.y -= (event.relative.x * sensitivity)
 		look_rot.x -= (event.relative.y * sensitivity)
 		look_rot.x = clamp(look_rot.x, min_angle, max_angle)
@@ -96,13 +96,12 @@ func crouch(delta : float, reverse = false):
 
 
 func hurt(damage : float):
-	health_bar.value -= damage
-	GameState.set_value("health", health_bar.value)
-	hurt_overlay.modulate = Color.WHITE
-	if hurt_tween:
-		hurt_tween.kill()
-	hurt_tween = create_tween()
-	hurt_tween.parallel().tween_property(hurt_overlay, "modulate", Color.TRANSPARENT, 0.5)
+	ui.hurt(damage)
 	
-	hurt_tween.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
-	hurt_tween.parallel().tween_property(health_bar_bg, "value", health_bar.value, 0.6)
+	if ui.health_bar.value <= 0:
+		die()
+
+
+func die():
+	moving = false
+	ui.show_gameover()
---
+++ Player/ui.gd
@@ -0,0 +1,42 @@
+extends Control
+
+@onready var hurt_overlay = $HurtOverlay
+@onready var health_bar_bg = $HealthBarBG
+@onready var health_bar = $HealthBar
+@onready var game_over = $GameOver
+
+var hurt_tween : Tween
+
+
+func _ready():
+	health_bar.value = GameState.get_value("health")
+	health_bar_bg.value = health_bar.value
+	game_over.hide()
+
+
+func hurt(damage: float):
+	health_bar.value -= damage
+	GameState.set_value("health", health_bar.value)
+	
+	hurt_overlay.modulate = Color.WHITE
+	if hurt_tween:
+		hurt_tween.kill()
+	hurt_tween = create_tween()
+	hurt_tween.parallel().tween_property(hurt_overlay, "modulate", Color.TRANSPARENT, 0.5)
+	
+	hurt_tween.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+	hurt_tween.parallel().tween_property(health_bar_bg, "value", health_bar.value, 0.6)
+
+
+func show_gameover():
+	game_over.show()
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func _on_restart_button_pressed():
+	GameState.set_value("health", 100)
+	get_tree().reload_current_scene()
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
